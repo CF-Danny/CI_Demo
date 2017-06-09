9273bd714662ed3b751c6a8280caed62be2a36d8
Danny <danny@clickfunnels.com>
2017-06-09 15:10:53 -0400

    Create test_webcalc.py

--- test_webcalc.py
+++ test_webcalc.py
@@ -1,8 +1,21 @@
+""This module does tests"""
+
 import pytest
 from expecter import expect
 
-from webcalc import app
+from webcalc import app, mongo
 
+
+@pytest.fixture
+def pattern():
+    with app.app_context():
+        mongo.db.operations.drop()
+        mongo.db.operations.insert(
+            dict(
+                name="x",
+                pattern="{{ a * b }}"
+            )
+        )
 
 @pytest.fixture
 def client():
@@ -14,6 +27,18 @@ def describe_index():
     def it_says_hello(client):
         response = client.get('/')
 
-        expect(response.data).contains(b"Helo, world!")
+        expect(response.data).contains(b"Hello, world!")
+
+def describe_calc():
+
+        def when_plus(client):
+            response = client.get('/4/+/5')
+
+            expect(response.data).contains(b"9")
 
 
+        def from_db(client, pattern):
+            response = client.get('/4/x/5')
+
+            expect(response.data).contains(b"20")
+Contact GitHub 
